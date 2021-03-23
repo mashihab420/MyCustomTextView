a54c3944f7cb08619f8bd509e12b7804432c2390
Shihab <mahbubul15-8821@diu.edu.bd>
2021-03-23 11:39:15 +0600

    CustomEditText second

---
+++ mylibraryutils/src/main/java/com/rightbrain/mylibraryutils/CustomEditText.java
@@ -0,0 +1,67 @@
+package com.rightbrain.mylibraryutils;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+
+public class CustomEditText extends androidx.appcompat.widget.AppCompatEditText{
+    Typeface tf;
+    public CustomEditText(@NonNull Context context) {
+        super(context);
+
+        tf = ResourcesCompat.getFont(context, R.font.montserrat);
+        this.setTypeface(tf);
+        int padding = (int) getResources().getDimension(R.dimen.text_padding);
+        this.setTextSize( getResources().getDimension(R.dimen.text_size));
+        this.setTextColor(Color.parseColor("#1a1a1a"));
+        this.setHintTextColor(Color.parseColor("#FFF44336"));
+
+
+        this.setPadding(
+                padding,
+                padding,
+                padding,
+                padding
+        );
+
+    }
+
+    public CustomEditText(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        tf = ResourcesCompat.getFont(context, R.font.montserrat);
+        this.setTypeface(tf);
+        int padding = (int) getResources().getDimension(R.dimen.text_padding);
+        this.setTextSize( getResources().getDimension(R.dimen.text_size));
+        this.setTextColor(Color.parseColor("#1a1a1a"));
+        this.setHintTextColor(Color.parseColor("#FFF44336"));
+        this.setPadding(
+                padding,
+                padding,
+                padding,
+                padding
+        );
+    }
+
+    public CustomEditText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        tf = ResourcesCompat.getFont(context, R.font.montserrat);
+        this.setTypeface(tf);
+        int padding = (int) getResources().getDimension(R.dimen.text_padding);
+        this.setTextSize( getResources().getDimension(R.dimen.text_size));
+        this.setTextColor(Color.parseColor("#1a1a1a"));
+        this.setHintTextColor(Color.parseColor("#FFF44336"));
+        this.setPadding(
+                padding,
+                padding,
+                padding,
+                padding
+        );
+
+
+    }
+}
