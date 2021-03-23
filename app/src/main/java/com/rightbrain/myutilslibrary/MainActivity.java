1b272a4cc63a1ca1a9a29262e594d674eb7d014c
Shihab <mahbubul15-8821@diu.edu.bd>
2021-03-23 10:43:38 +0600

    First commit

---
+++ app/src/main/java/com/rightbrain/myutilslibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rightbrain.myutilslibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
